406760cec86f6c57066d2c3c8bc58e9581d4fde2
Shoaib H <slimsho123@gmail.com>
2022-07-07 22:41:27 -0500

    Added header elements/social network icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const header = () => {
   return (
-    <div>header</div>
+    
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Shoaib Huq</h1>
+        <h5 className="text-light">Mobile Application Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src = {ME}/>
+        </div>
+
+        <a href="#contact" className = 'scroll_down'>Scroll Down</a>
+        </div>
+    </header>
   )
 }
 
