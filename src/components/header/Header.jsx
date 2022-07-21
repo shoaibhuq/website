1c4ab76e150dd9d48682be7f0bd0438c6c82c59c
Shoaib Huq <slimsho123@gmail.com>
2022-07-21 13:27:36 -0500

    header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,7 +19,7 @@ const header = () => {
           <img src = {ME}/>
         </div>
 
-        <a href="#contact" className = 'scroll_down'>Scroll Down</a>
+        <a href="#contact" className = 'scroll__down'>Scroll Down</a>
         </div>
     </header>
   )
